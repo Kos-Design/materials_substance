096f2a3f3b03f38a45430d0f2017ee9c6cb41bb5
Cosmin Planchon <info@kosmin.fr>
2023-03-24 01:38:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Blender Substance Texture Importer",
     "author": "Cosmin Planchon",
-    "version": (0, 2, 0),
+    "version": (0, 2, 1),
     "blender": (2, 82, 0),
     "location": "Properties > Material",
     "description": "Import & autoassign images from Substance Painter or similar 3D painting tools",
@@ -11,121 +11,106 @@ bl_info = {
     "category": "Material"}
 
 import bpy
-import importlib
-
 from bpy.props import (
     StringProperty, IntProperty, BoolProperty,
     PointerProperty, CollectionProperty,
     FloatProperty,FloatVectorProperty,
-    EnumProperty,
+    EnumProperty, 
 )
 
-from . import PropertyGroups
-
-from . import Operators
-
-from . import Panels
-
-importlib.reload(PropertyGroups)
-
-importlib.reload(Operators)
-
-importlib.reload(Panels)
-
-from . PropertyGroups import ( Paneline0, Paneline1, Paneline2, Paneline3,
-                            Paneline4, Paneline5, Paneline6, Paneline7,
-                            Paneline8, Paneline9,  ShaderLinks,
-                            NodesLinks, KosVars,
+from . propertygroups import ( PaneLine0, PaneLine1, PaneLine2, PaneLine3,
+                            PaneLine4, PaneLine5, PaneLine6, PaneLine7,
+                            PaneLine8, PaneLine9, ShaderLinks,
+                            NodesLinks, BSMprops,
                             )
 
-from . Operators import ( KOS_MT_presetsmenu, KosExecutePreset,
-                          KOS_OT_createnodes, KOS_OT_assignnodes,
-                          KOS_OT_checkmaps, KOS_OT_assumename,
-                          KOS_OT_createdummy,
-                          KOS_OT_saveall, KOS_OT_loadall,
-                          KOS_OT_subimport, KOS_OT_removeline,
-                          KOS_OT_addpreset,
-                          KOS_OT_addaline, KOS_OT_guessfilext,
+from . operators import ( BSM_MT_presetsmenu, BsmExecutePreset,
+                          BSM_OT_createnodes, BSM_OT_assignnodes,
+                          BSM_OT_checkmaps, BSM_OT_assumename,
+                          BSM_OT_createdummy,
+                          BSM_OT_saveall, BSM_OT_loadall,
+                          BSM_OT_subimport, BSM_OT_removeline,
+                          BSM_OT_addpreset,
+                          BSM_OT_addaline, BSM_OT_guessfilext,
                           )
 
-from . Panels import (  KOS_PT_presets, KOS_PT_importpanel, KOS_PT_linestopanel,
-                        KOS_PT_prefs, KOS_PT_options, KOS_PT_params, KOS_PT_buttons,
+from . panels import (  BSM_PT_presets, BSM_PT_importpanel, BSM_PT_linestopanel,
+                        BSM_PT_prefs, BSM_PT_options, BSM_PT_params, BSM_PT_buttons,
                         )
 
 classesp = (
-    KosVars,
-    ShaderLinks,
+    BSMprops,
     NodesLinks,
-    Paneline0,
-    Paneline1,
-    Paneline2,
-    Paneline3,
-    Paneline4,
-    Paneline5,
-    Paneline6,
-    Paneline7,
-    Paneline8,
-    Paneline9,
-    KOS_OT_addaline,
-    KosExecutePreset,
-    KOS_OT_createdummy,
-    KOS_OT_guessfilext,
-    #KOS_OT_realinit,
-    KOS_OT_assumename,
-    KOS_OT_checkmaps,
-    KOS_OT_createnodes,
-    KOS_MT_presetsmenu,
-    KOS_OT_addpreset,
-    KOS_PT_presets,
-    KOS_OT_subimport,
-    KOS_PT_importpanel,
-    KOS_PT_params,
-    KOS_PT_linestopanel,
-    KOS_PT_prefs,
-    KOS_PT_buttons,
-    KOS_PT_options,
-    KOS_OT_removeline,
-    KOS_OT_saveall,
-    KOS_OT_loadall,
-    KOS_OT_assignnodes,
+    ShaderLinks,
+    PaneLine0,
+    PaneLine1,
+    PaneLine2,
+    PaneLine3,
+    PaneLine4,
+    PaneLine5,
+    PaneLine6,
+    PaneLine7,
+    PaneLine8,
+    PaneLine9,
+    BSM_OT_addaline,
+    BsmExecutePreset,
+    BSM_OT_createdummy,
+    BSM_OT_guessfilext,
+    BSM_OT_assumename,
+    BSM_OT_checkmaps,
+    BSM_OT_createnodes,
+    BSM_MT_presetsmenu,
+    BSM_OT_addpreset,
+    BSM_PT_presets,
+    BSM_OT_subimport,
+    BSM_PT_importpanel,
+    BSM_PT_params,
+    BSM_PT_linestopanel,
+    BSM_PT_prefs,
+    BSM_PT_buttons,
+    BSM_PT_options,
+    BSM_OT_removeline,
+    BSM_OT_saveall,
+    BSM_OT_loadall,
+    BSM_OT_assignnodes,
     )
 
 def register():
     from bpy.utils import register_class
     for cls in classesp:
         register_class(cls)
-
-    bpy.types.Scene.kosvars = PointerProperty(type=KosVars)
-    bpy.types.Scene.kosp0 = PointerProperty(type=Paneline0)
-    bpy.types.Scene.kosp1 = PointerProperty(type=Paneline1)
-    bpy.types.Scene.kosp2 = PointerProperty(type=Paneline2)
-    bpy.types.Scene.kosp3 = PointerProperty(type=Paneline3)
-    bpy.types.Scene.kosp4 = PointerProperty(type=Paneline4)
-    bpy.types.Scene.kosp5 = PointerProperty(type=Paneline5)
-    bpy.types.Scene.kosp6 = PointerProperty(type=Paneline6)
-    bpy.types.Scene.kosp7 = PointerProperty(type=Paneline7)
-    bpy.types.Scene.kosp8 = PointerProperty(type=Paneline8)
-    bpy.types.Scene.kosp9 = PointerProperty(type=Paneline9)
-    bpy.types.Scene.kosni = CollectionProperty(type=NodesLinks)
-    bpy.types.Scene.koshi = CollectionProperty(type=ShaderLinks)
+   
+    bpy.types.Scene.bsmprops = PointerProperty(type=BSMprops)
+    bpy.types.Scene.panel_line0 = PointerProperty(type=PaneLine0)
+    bpy.types.Scene.panel_line1 = PointerProperty(type=PaneLine1)
+    bpy.types.Scene.panel_line2 = PointerProperty(type=PaneLine2)
+    bpy.types.Scene.panel_line3 = PointerProperty(type=PaneLine3)
+    bpy.types.Scene.panel_line4 = PointerProperty(type=PaneLine4)
+    bpy.types.Scene.panel_line5 = PointerProperty(type=PaneLine5)
+    bpy.types.Scene.panel_line6 = PointerProperty(type=PaneLine6)
+    bpy.types.Scene.panel_line7 = PointerProperty(type=PaneLine7)
+    bpy.types.Scene.panel_line8 = PointerProperty(type=PaneLine8)
+    bpy.types.Scene.panel_line9 = PointerProperty(type=PaneLine9)
+    bpy.types.Scene.node_links = CollectionProperty(type=NodesLinks)
+    bpy.types.Scene.shader_links = CollectionProperty(type=ShaderLinks)
 
 def unregister():
     from bpy.utils import unregister_class
     for cls in classesp:
         unregister_class(cls)
-    del bpy.types.Scene.kosni
-    del bpy.types.Scene.koshi
-    del bpy.types.Scene.kosp0
-    del bpy.types.Scene.kosp1
-    del bpy.types.Scene.kosp2
-    del bpy.types.Scene.kosp3
-    del bpy.types.Scene.kosp4
-    del bpy.types.Scene.kosp5
-    del bpy.types.Scene.kosp6
-    del bpy.types.Scene.kosp7
-    del bpy.types.Scene.kosp8
-    del bpy.types.Scene.kosp9
-    del bpy.types.Scene.kosvars
+    del bpy.types.Scene.shader_links
+    del bpy.types.Scene.node_links
+    del bpy.types.Scene.panel_line9
+    del bpy.types.Scene.panel_line8
+    del bpy.types.Scene.panel_line7
+    del bpy.types.Scene.panel_line6
+    del bpy.types.Scene.panel_line5
+    del bpy.types.Scene.panel_line4
+    del bpy.types.Scene.panel_line3
+    del bpy.types.Scene.panel_line2
+    del bpy.types.Scene.panel_line1
+    del bpy.types.Scene.panel_line0
+    del bpy.types.Scene.bsmprops
 
 if __name__ == '__main__':
     register()
