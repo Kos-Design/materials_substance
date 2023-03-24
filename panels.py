157d52c40a05c04b914789ddf90bbaaf9501685f
Cosmin Planchon <info@kosmin.fr>
2023-03-24 01:35:46 +0100

    Update and rename Panels.py to panels.py

--- panels.py
+++ panels.py
@@ -20,55 +20,55 @@ class TexImporterPanel():
         return False
 
 
-class KOS_PT_presets(PresetPanel, Panel):
+class BSM_PT_presets(PresetPanel, Panel):
     bl_label = 'Presets'
-    preset_subdir = 'kospresets'
-    preset_operator = 'kos.execute_preset'
-    preset_add_operator = 'kos.addpreset'
+    preset_subdir = 'bsm_presets'
+    preset_operator = 'bsm.execute_preset'
+    preset_add_operator = 'bsm.addpreset'
 
-class KOS_PT_importpanel(TexImporterPanel, Panel):
-    bl_idname = "KOS_PT_importpanel"
+class BSM_PT_importpanel(TexImporterPanel, Panel):
+    bl_idname = "BSM_PT_importpanel"
     bl_label = "Substance Texture Importer"
 
     def draw_header_preset(self, _context):
         layout = self.layout
-        KOS_PT_presets.draw_panel_header(self.layout)
+        BSM_PT_presets.draw_panel_header(self.layout)
 
     def draw(self, context):
         layout = self.layout
 
-class KOS_PT_params(TexImporterPanel, Panel):
+class BSM_PT_params(TexImporterPanel, Panel):
 
-    bl_idname = "KOS_PT_params"
+    bl_idname = "BSM_PT_params"
     bl_label = "Maps Folder:"
-    bl_parent_id = "KOS_PT_importpanel"
+    bl_parent_id = "BSM_PT_importpanel"
 
     def draw(self, context):
         layout = self.layout
         scene = bpy.context.scene
-        kosvars = scene.kosvars
+        bsmprops = scene.bsmprops
         row = layout.row()
-        row.prop(kosvars, "kosdir")
+        row.prop(bsmprops, "usr_dir")
 
-class KOS_PT_linestopanel(TexImporterPanel, Panel):
-    bl_idname = "KOS_PT_linestopanel"
+class BSM_PT_linestopanel(TexImporterPanel, Panel):
+    bl_idname = "BSM_PT_linestopanel"
     bl_label = "Texture maps"
-    bl_parent_id = "KOS_PT_importpanel"
+    bl_parent_id = "BSM_PT_importpanel"
     bl_options = {'HIDE_HEADER'}
 
 
     def draw(self, context):
         layout = self.layout
         scene = bpy.context.scene
-        kosvars = scene.kosvars
-        expertmode = kosvars.expert
-        panelrows = kosvars.panelrows
+        bsmprops = scene.bsmprops
+        expertmode = bsmprops.expert
+        panelrows = bsmprops.panelrows
         row = layout.row()
         col = row.column()
         col.alignment = 'RIGHT'
         row = col.row()
         row.alignment = 'RIGHT'
-        row.operator("KOS_OT_addaline", icon="ADD")
+        row.operator("BSM_OT_addaline", icon="ADD")
         row.separator()
         row = layout.row(align=True)
         row = row.split(factor=.02)
@@ -96,143 +96,143 @@ class KOS_PT_linestopanel(TexImporterPanel, Panel):
         row = layout.row()
 
         for k in range(panelrows):
-            kosp = eval(f"scene.kosp{k}")
-            lefilename = kosp.lefilename
-            manual = kosp.manual
-            lefile = os.path.basename(kosp.probable)
-            dircontent = os.listdir(kosvars.kosdir)
+            panel_line = eval(f"scene.panel_line{k}")
+            lefilename = panel_line.lefilename
+            manual = panel_line.manual
+            lefile = os.path.basename(panel_line.probable)
+            dircontent = os.listdir(bsmprops.usr_dir)
             if manual:
                 lefile = os.path.basename(lefilename)
-                dircontent = kosvars.dircontent
+                dircontent = bsmprops.dircontent
 
             row = layout.row(align = True)
-            row.prop(kosp, "labelbools", text = "")
+            row.prop(panel_line, "labelbools", text = "")
             row.use_property_split = True
             row.use_property_decorate = False
             row = row.split(factor = .35)
-            row.active = kosp.labelbools
-            row.enabled = kosp.labelbools
+            row.active = panel_line.labelbools
+            row.enabled = panel_line.labelbools
             row.alignment = 'LEFT'
             col = row.column(align=True)
             if manual :
-                col.prop(kosp, "lefilename", text = "")
+                col.prop(panel_line, "lefilename", text = "")
             else:
-                col.prop(kosp, "maplabels", text = "")
+                col.prop(panel_line, "maplabels", text = "")
             row = row.split(factor = .11)
             row.alignment = 'EXPAND'
             col = row.column(align = True)
             col.alignment = 'EXPAND'
             if not expertmode:
                 if lefile in dircontent:
-                    col.operator('KOS_OT_checkmaps', icon='CHECKMARK').linen = k
+                    col.operator('BSM_OT_checkmaps', icon='CHECKMARK').linen = k
                 else:
-                    col.operator('KOS_OT_checkmaps', icon='QUESTION').linen = k
+                    col.operator('BSM_OT_checkmaps', icon='QUESTION').linen = k
             else :
 
-                col.prop(kosp, "manual", text="", toggle=1, icon='FILE_TICK')
+                col.prop(panel_line, "manual", text="", toggle=1, icon='FILE_TICK')
             row = row.split(factor=.33)
             row.alignment = 'LEFT'
             col = row.column(align=True)
             col.alignment = 'LEFT'
             col.enabled = not manual
-            col.prop(kosp, "mapext", text="")
+            col.prop(panel_line, "mapext", text="")
             row = row.split(factor=.97)
             col = row.column()
-            col.enabled = kosp.labelbools
+            col.enabled = panel_line.labelbools
             if not col.active :
-                col.prop(kosvars, "emptylist" , text="")
+                col.prop(bsmprops, "emptylist" , text="")
             else:
-                col.prop(kosp, "inputsockets" , text="")
+                col.prop(panel_line, "inputsockets" , text="")
             row.separator()
         row = layout.row()
 
-class KOS_PT_prefs(TexImporterPanel,Panel):
+class BSM_PT_prefs(TexImporterPanel,Panel):
 
-    bl_idname = "KOS_PT_prefs"
+    bl_idname = "BSM_PT_prefs"
     bl_label = "Preferences"
-    bl_parent_id = "KOS_PT_importpanel"
+    bl_parent_id = "BSM_PT_importpanel"
     bl_options = {'DEFAULT_CLOSED',}
 
     def draw_header_preset(self, _context):
         layout = self.layout
         row = layout.row()
         row = row.split(factor=.95)
-        row.operator("KOS_OT_removeline", icon="REMOVE",)
+        row.operator("BSM_OT_removeline", icon="REMOVE",)
 
 
     def draw(self, context):
         layout = self.layout
         scene = bpy.context.scene
-        kosvars = scene.kosvars
+        bsmprops = scene.bsmprops
         row = layout.row()
         row.alignment = 'LEFT'
         row.split(factor=1, align = True)
-        row.prop(kosvars, "prefix", text="Prefix")
+        row.prop(bsmprops, "prefix", text="Prefix")
         row.split(factor=.1)
         row.split(factor=.1)
-        row.prop(kosvars, "separator", text="Separator")
+        row.prop(bsmprops, "separator", text="Separator")
         box = layout.box()
         row = box.row()
         row.label(text="Pattern of the files in the Maps Folder : ")
         row = box.row()
-        row.prop(kosvars, "patterns", text="")
+        row.prop(bsmprops, "patterns", text="")
         row = layout.row()
-        row.prop(kosvars, "expert", text="Advanced Mode ", )
+        row.prop(bsmprops, "expert", text="Advanced Mode ", )
 
-class KOS_PT_buttons(TexImporterPanel, Panel):
+class BSM_PT_buttons(TexImporterPanel, Panel):
 
-    bl_idname = "KOS_PT_buttons"
+    bl_idname = "BSM_PT_buttons"
     bl_label = "Options"
-    bl_parent_id = "KOS_PT_importpanel"
+    bl_parent_id = "BSM_PT_importpanel"
     bl_options = {'HIDE_HEADER'}
 
     def draw(self, context):
         layout = self.layout
         row = layout.row()
         col = row.column(align = True)
-        col.operator("KOS_OT_subimport")
+        col.operator("BSM_OT_subimport")
         col.separator()
 
         row = col.row(align = True)
         col = layout.column(align = True)
         row = col.row(align = True)
-        row.operator("KOS_OT_assignnodes")
+        row.operator("BSM_OT_assignnodes")
         row.separator()
-        row.operator("KOS_OT_createnodes")
+        row.operator("BSM_OT_createnodes")
 
 
-class KOS_PT_options(TexImporterPanel, Panel):
+class BSM_PT_options(TexImporterPanel, Panel):
 
-    bl_idname = "KOS_PT_options"
+    bl_idname = "BSM_PT_options"
     bl_label = "Options"
-    bl_parent_id = "KOS_PT_importpanel"
+    bl_parent_id = "BSM_PT_importpanel"
 
     def draw(self, context):
         layout = self.layout
-        kosvars = bpy.context.scene.kosvars
+        bsmprops = bpy.context.scene.bsmprops
         row = layout.row()
         col = row.column(align = True)
         row = col.row(align = True)
-        row.prop(kosvars, "shader", text="Replace Shader",)
+        row.prop(bsmprops, "shader", text="Replace Shader",)
         row = row.split()
-        row.prop(kosvars, "shaderlist", text="")
+        row.prop(bsmprops, "shaderlist", text="")
         row = layout.row()
         row = layout.row()
-        row.enabled = not kosvars.manison
-        row.prop(kosvars, "applyall", text="Apply to all visible objects", )
+        row.enabled = not bsmprops.manison
+        row.prop(bsmprops, "applyall", text="Apply to all visible objects", )
         row = layout.row()
-        row.enabled = not kosvars.manison
-        row.prop(kosvars, "onlyactiveobj", text="Apply to active object only", )
+        row.enabled = not bsmprops.manison
+        row.prop(bsmprops, "onlyactiveobj", text="Apply to active object only", )
         row = layout.row()
-        row.prop(kosvars, "skipnormals", )
+        row.prop(bsmprops, "skipnormals", )
         row = layout.row()
-        row.prop(kosvars, "customshader", text="Enable Custom Shaders", icon='NONE', icon_only=True)
+        row.prop(bsmprops, "customshader", text="Enable Custom Shaders", icon='NONE', icon_only=True)
         row = layout.row()
-        row.prop(kosvars, "eraseall", text="Clear nodes from material", )
+        row.prop(bsmprops, "eraseall", text="Clear nodes from material", )
         row = layout.row()
-        row.prop(kosvars, "extras", text="Attach Curves and Ramps ", )
+        row.prop(bsmprops, "extras", text="Attach Curves and Ramps ", )
         row = layout.row()
-        row.enabled = not kosvars.manison
-        row.prop(kosvars, "onlyamat", text="Only active Material",)
+        row.enabled = not bsmprops.manison
+        row.prop(bsmprops, "onlyamat", text="Only active Material",)
         row = layout.row()
-        row.prop(kosvars, "fixname", text="Fix copied Material name",)
+        row.prop(bsmprops, "fixname", text="Fix copied Material name",)
